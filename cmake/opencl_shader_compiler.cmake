92ecefac27e34310ec7631c6fd97443f1ee77ded
Robert Quill <robert.quill@imgtec.com>
2024-05-09 12:34:43 +0100

    Fix support for using clspv in CMake
    It's not actually useful in Kompute because the pipeline creation
    expects the entry-point to be called main, but it's actually the kernel
    name.
    
    Commit it for now as a record of work done. Can be reverted later.

--- cmake/opencl_shader_compiler.cmake
+++ cmake/opencl_shader_compiler.cmake
@@ -5,8 +5,11 @@ function(opencl_compile_shader)
           return()
      endif()
 
-     cmake_parse_arguments(SHADER_COMPILE "" "INFILE;OUTFILE;NAMESPACE;RELATIVE_PATH" "" ${ARGN})
+     set(oneValueArgs INFILE OUTFILE NAMESPACE RELATIVE_PATH VARIABLE_NAME)
+     set(multiValueArgs ARGS)
+     cmake_parse_arguments(SHADER_COMPILE "" "${oneValueArgs}" "${multiValueArgs}" ${ARGN} )
      set(SHADER_COMPILE_INFILE_FULL "${CMAKE_CURRENT_SOURCE_DIR}/${SHADER_COMPILE_INFILE}")
+     set(SHADER_COMPILE_SPV_FILE_FULL "${CMAKE_CURRENT_BINARY_DIR}/${SHADER_COMPILE_INFILE}.spv")
      set(SHADER_COMPILE_HEADER_FILE_FULL "${CMAKE_CURRENT_BINARY_DIR}/${SHADER_COMPILE_OUTFILE}")
 
      if(NOT SHADER_COMPILE_RELATIVE_PATH)
@@ -14,12 +17,30 @@ function(opencl_compile_shader)
      endif()
     
      # .cl -> .sh
-     add_custom_command(OUTPUT "${SHADER_COMPILE_HEADER_FILE_FULL}"
+     add_custom_command(OUTPUT "${SHADER_COMPILE_SPV_FILE_FULL}"
                         COMMAND "${CLSPV_PATH}"
-                        ARGS "-mfmt=c"
+                        ARGS ${SHADER_COMPILE_ARGS}
                              "${SHADER_COMPILE_INFILE_FULL}"
                              "-o"
-                             "${SHADER_COMPILE_HEADER_FILE_FULL}"
+                             "${SHADER_COMPILE_SPV_FILE_FULL}"
                         COMMENT "Compile OpenCL compute shader from file '${SHADER_COMPILE_INFILE_FULL}' to '${SHADER_COMPILE_SPV_FILE_FULL}'."
                         MAIN_DEPENDENCY "${SHADER_COMPILE_INFILE_FULL}")
+
+     # Check if big or little endian
+     include (TestBigEndian)
+     TEST_BIG_ENDIAN(IS_BIG_ENDIAN)
+
+     # .spv -> .hpp
+     add_custom_command(OUTPUT "${SHADER_COMPILE_HEADER_FILE_FULL}"
+                        COMMAND ${CMAKE_COMMAND}
+                        ARGS "-DINPUT_SHADER_FILE=${SHADER_COMPILE_SPV_FILE_FULL}"
+                             "-DOUTPUT_HEADER_FILE=${SHADER_COMPILE_HEADER_FILE_FULL}"
+                             "-DHEADER_NAMESPACE=${SHADER_COMPILE_NAMESPACE}"
+                             "-DIS_BIG_ENDIAN=${IS_BIG_ENDIAN}"
+                             "-DVARIABLE_NAME=${SHADER_COMPILE_VARIABLE_NAME}"
+                             "-P"
+                             "${SHADER_COMPILE_RELATIVE_PATH}/bin_file_to_header.cmake"
+                        WORKING_DIRECTORY "${SHADER_COMPILE_RELATIVE_PATH}"
+                        COMMENT "Converting compiled shader '${SHADER_COMPILE_SPV_FILE_FULL}' to header file '${SHADER_COMPILE_HEADER_FILE_FULL}'."
+                        MAIN_DEPENDENCY "${SHADER_COMPILE_SPV_FILE_FULL}")
 endfunction()
